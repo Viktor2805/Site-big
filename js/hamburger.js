901ac86ec3d85d5999794a925f696772cc0aeb67
harevicviktor#gmail.com <harevicviktor@gmail.com>
2020-07-09 19:01:00 +0300

    delete hamburger

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,14 +3,14 @@ const hamburger = document.querySelector(".section-header__humburger");
 const headerWrapper = document.querySelector(".section-header-wrapper__nav");
 const button = document.querySelector(".section-header-wrapper__button");
 
-hamburger.addEventListener("click", () => {
-    if (headerWrapper.style.display === "flex") {
-        headerWrapper.style.display = "none";
-        button.style.display = "none";
-    } else {
-        headerWrapper.style.display = "flex";
-        button.style.display = "block";
-    }
-})
+// hamburger.addEventListener("click", () => {
+//     if (headerWrapper.style.display === "flex") {
+//         headerWrapper.style.display = "none";
+//         button.style.display = "none";
+//     } else {
+//         headerWrapper.style.display = "flex";
+//         button.style.display = "block";
+//     }
+// })
 // ===  hamburger  === //
 
